6b0d847ba7a67a1676b9652c1544cf6f3671e10b
viccky8578 <manbodhvikram@gmail.com>
2023-02-08 13:02:43 +0530

    first code of method overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
-}
+        public static void main(String[] args) {
+            A a1 = new A();
+            a1.meth();
+            B b1 = new B();
+            b1.meth();
+
+
+        }
+    }
