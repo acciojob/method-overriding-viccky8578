425c1fb4ec4ceab0f51454d7788c9a6988962b96
viccky8578 <manbodhvikram@gmail.com>
2023-02-08 13:11:30 +0530

    first code of method overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,22 @@
 package com.driver;
 
 public class Main {
+    public static class A
+    {
+       public String meth()
+        {
+            return " Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public String meth()
+        {
+            return " Method is overridden in Extended class B";
+        }
+    }
         public static void main(String[] args) {
-            A a1 = new A();
-            a1.meth();
+            B b = new B();
+            b.meth();
             B b1 = new B();
             b1.meth();
 
