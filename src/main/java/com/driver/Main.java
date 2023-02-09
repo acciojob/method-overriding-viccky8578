9521f27f81b19f5f70c8b50f98c5c566740f4bc0
viccky8578 <manbodhvikram@gmail.com>
2023-02-10 00:37:00 +0530

    code of methid overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,14 +1,16 @@
 package com.driver;
 
 public class Main {
-    public static class A
+    public   static class A
     {
+
        public String meth()
         {
-            return " Invoking method from class A";
+            return "Invoking method from class A";
         }
     }
-    public static class B extends A{
+    public static  class B extends A{
+        @Override
         public String meth()
         {
             return " Method is overridden in Extended class B";
