d50565d8d6048feabbdf52abcadc8f9d9c6b42e6
viccky8578 <manbodhvikram@gmail.com>
2023-02-10 13:56:02 +0530

    code of method Overriding

---
+++ src/main/java/com.driver/Main.java
@@ -0,0 +1,24 @@
+package com.driver;
+
+public class Main {
+    public static class A{
+        public String meth()
+        {
+            return "Invoking mthod class from A";
+        }
+    }
+    public static class B extends A{
+        public String meth()
+        {
+            return "Method is overridden in Extended class B";
+
+        }
+    }
+    public static void main(String[] args)
+    {
+        B b=new B();
+        b.meth();
+
+
+    }
+}
